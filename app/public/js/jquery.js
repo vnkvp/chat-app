5244bf70c0e111d353ebbe1e84fd7b0420f8b847
vnkvp <vnkvp@outlook.com>
2019-06-21 21:33:45 -0300

    backendset

--- app/public/js/jquery.js
+++ app/public/js/jquery.js
@@ -3,6 +3,11 @@ $(() => {
     var $messageForm = $('#messageForm');
     var $message = $('#message');
     var $chat = $('#chat');
+    var $userForm = $('#userForm');
+    var $userFormArea = $('#userFormArea');
+    var $messageArea = $('#messageArea');
+    var $users = $('#users');
+    var $username = $('#username');
 
     $messageForm.submit((e) => {
         e.preventDefault();
@@ -10,7 +15,26 @@ $(() => {
         $message.val('');
     });
 
-    socket.on('new message', (data)=>{
-$chat.append('<div class="well">'+data.msg+'</div>');
+    socket.on('new message', (data) => {
+        $chat.append('<div class="well"><strong>'+data.user+': </strong>' + data.msg + '</div>');
+    });
+
+    $userForm.submit((e) => {
+        e.preventDefault();
+        socket.emit('new user', $username.val(), (data) => {
+            if (data) {
+                $userFormArea.hide();
+                $messageArea.show();
+            }
+        });
+        $username.val('');
+    });
+    
+    socket.on('get users', (data)=>{
+var html = '';
+for (i = 0;i < data.length;i++){
+    html += '<li class="list-group-item">'+data[i]+'</li>';
+}
+$users.html(html);
     });
 });
--- server.js
+++ server.js
@@ -17,12 +17,25 @@ connections.push(socket);
 console.log('Connected: %s sockets connected', connections.length);
 
 socket.on('disconnect', (data) => {
+    
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
     connections.splice(connections.indexOf(socket), 1);
     console.log('Disconnected: %s sockets connected', connections.length);    
 });
 
 socket.on('send message', (data) => {
-io.sockets.emit('new message', {msg: data});
+io.sockets.emit('new message', {msg: data, user: socket.username});
 });
+
+socket.on('new user', (data, callback) => {
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+});
+function updateUsernames() {
+    io.sockets.emit('get users', users);
+}
 
 });
